1d8c88eb908ba2d206dfacd31940f393bc33f27e
shamim1999 <38599943+shamim1999@users.noreply.github.com>
2020-03-21 17:25:09 +0600

    Null vs undefind preparation done

---
+++ null-undefined.js
@@ -0,0 +1,11 @@
+function add(num1,num2){
+    console.log(num1, num2);
+}
+
+const premik ={name:"smart dude",phone:45435};
+console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+let ages=[2,5,9]
+console.log(ages[11]);
