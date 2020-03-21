37c09c843f944c6b52fa7bea794acc6761512039
shamim1999 <38599943+shamim1999@users.noreply.github.com>
2020-03-21 18:23:23 +0600

    onek kisu shikhlam

---
+++ object-property.js
@@ -0,0 +1,14 @@
+const students=[
+    {id:21, name:'Omor Sunny'},
+    {id:31, name:'mannaaaaaa'},
+    {id:41, name:'Moyouri'},
+    {id:71, name:'DeepJol'}
+];
+
+const names = students.map(s=>s.name);
+const ids= students.map(s=>s.id);
+const bigger= students.filter(s => s.id>40);
+const biggerOne=students.find(s=>s.id>40);
+console.log(names);
+console.log(bigger);
+console.log(biggerOne);
