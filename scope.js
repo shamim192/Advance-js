ff4bd9fc19405c559d153a7b843b4e09c3e609c0
shamim1999 <38599943+shamim1999@users.noreply.github.com>
2020-03-21 22:06:46 +0600

    scope done

---
+++ scope.js
@@ -0,0 +1,21 @@
+
+let bonus=20;
+
+function sum(first,second){
+    let result = first + second + bonus;
+    console.log(bonus);
+    if(result>9){
+        var mood="happpy";
+        mood="fishy";
+        mood="funcky";
+        mood="cranky";
+        //console.log(mood);
+    }
+    console.log(day);
+    var day="friday"
+    return result;
+}
+ 
+const output =sum(3,7);
+console.log(bonus);
+console.log(output);
